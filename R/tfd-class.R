41c10edd936db917479a965ed79dfb9863fccbca
Fabian Scheipl <fabian.scheipl@googlemail.com>
2018-07-26 13:49:21 +0200

    never create tfd's with NA evaluations

--- R/tfd-class.R
+++ R/tfd-class.R
@@ -168,11 +168,25 @@ tfd.list <- function(data, arg = NULL, domain = NULL,
 tfd.tf <- function(data, arg = NULL, domain = NULL, 
     evaluator = approx_linear, signif = NULL, ...) {
   evaluator <- quo_name(enexpr(evaluator))
+  domain <- domain %||% range(unlist(arg)) %||% domain(data)
+  signif <- signif %||% attr(data, "signif_arg")
   arg <- ensure_list(arg %||% arg(data))
   evaluations <- evaluate(data, arg)
+  nas <- map(evaluations, ~ which(is.na(.x)))
+  if (length(unlist(nas))) {
+    warning("NAs created, returned object will be irregular")
+    evaluations <- map2(evaluations, nas, ~ {
+      if(length(.y)) {
+        .x[-.y]
+      } else .x})
+    arg <- map2(arg, nas, ~ {
+      if(length(.y)) {
+        .x[-.y]
+      } else .x})
+  }
   names(evaluations) <- names(data)
-  domain <- domain %||% domain(data)
-  signif <- signif %||% attr(data, "signif_arg")
+  # FIXME : this does not check for NAs in evaluations! 
+  # (especialy leading/closing NAs could happen often)
   new_tfd(arg, evaluations, regular = (length(arg) == 1),
     domain = domain, evaluator = evaluator, 
     signif = signif)
